4e8d7ab2b67d0aece0416f7e0e010e9c327f33e8
Mike Hummel <mike@mhus.de>
2020-06-07 20:41:24 +0200

    Update SimLotto.java

--- sim-lotto/SimLotto.java
+++ sim-lotto/SimLotto.java
@@ -1,17 +1,119 @@
 import java.util.HashMap;
-import java.util.LinkedList;
 
 public class SimLotto {
 	
 	
 	public static void main(String[] args) {
-		new SimLotto().run3();
+		new SimLotto().run7();
 	}
 
 	private long round;
 	private long sum;
 	private boolean quiet;
 
+	
+	private void run7() {
+		quiet = true;
+		long roundSum = 0;
+		long roundMax = 0;
+		long roundMin = Long.MAX_VALUE;
+		
+		for (int i = 1; i < 1000; i++) {
+			run4();
+			roundSum = roundSum + round;
+			roundMax = Math.max(roundMax, round);
+			roundMin = Math.min(roundMin, round);
+			System.out.println(i + ": " + round + " \t" + sum + " \t| " + (roundSum / i) + " \t| " + roundMin + " - " + roundMax);
+		}
+	}
+	
+	private void run6() {
+		quiet = true;
+		for (int i = 0; i < 1000; i++) {
+			run5();
+			System.out.println(i + ": " + round + "  \t" + sum);
+		}
+	}
+	
+	/**
+	 * Calculate round until first win
+	 */
+	private void run5() {
+
+		HashMap<String, Integer> in = new HashMap<>();
+		in.put("true 2", 5);
+		in.put("false 3", 12);
+		in.put("true 3", 30);
+		in.put("false 4", 50);
+		in.put("true 4", 300);
+		in.put("false 5", 4183);
+		in.put("true 5", 19552);
+		in.put("false 6", 1994315);
+		in.put("true 6", 26887087);
+		
+		LottoResult result = new LottoResult(5,18,33,35,42,43,0);
+		round = 1;
+		
+		sum = 0;
+		long cost = 2;
+		
+		while (true) {
+			LottoResult tip = new LottoResult();
+			sum = sum - cost;
+			int equ = result.compareNumbers(tip);
+			boolean sup = tip.sup == result.sup;
+			Integer ini = in.get(sup + " " + equ);
+			if (ini != null)
+				sum = sum + ini.longValue();
+			if (!quiet)
+				System.out.println(round + ": " + sup + " " + equ + " --- " + sum);
+			if (ini != null) {
+				break;
+			}
+			round++;
+		}
+	}
+		
+	/**
+	 * Calculate round until zero
+	 */
+	private void run4() {
+
+		HashMap<String, Integer> in = new HashMap<>();
+		in.put("true 2", 5);
+		in.put("false 3", 12);
+		in.put("true 3", 30);
+		in.put("false 4", 50);
+		in.put("true 4", 300);
+		in.put("false 5", 4183);
+		in.put("true 5", 19552);
+		in.put("false 6", 1994315);
+		in.put("true 6", 26887087);
+		
+		LottoResult result = new LottoResult(5,18,33,35,42,43,0);
+		round = 1;
+		
+		sum = 0;
+		long cost = 2;
+		
+		while (true) {
+			LottoResult tip = new LottoResult();
+			sum = sum - cost;
+			int equ = result.compareNumbers(tip);
+			boolean sup = tip.sup == result.sup;
+			Integer ini = in.get(sup + " " + equ);
+			if (ini != null)
+				sum = sum + ini.longValue();
+			if (!quiet)
+				System.out.println(round + ": " + sup + " " + equ + " --- " + sum);
+			if (sum >= 0) {
+				break;
+			}
+			round++;
+		}
+	}
+	
+	
 	private void run3() {
 		quiet = true;
 		long roundSum = 0;
